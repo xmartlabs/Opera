ec8083de0be8397f35378e4fb0b652891ca5955c
Martin Barreto <martin@xmartlabs.com>
2016-04-12 01:49:59 -0300

    added ability to customize request parameters and mutableUrlRequest when working with PaginationRequestType using its RouteType.

--- Sources/PaginationRequestType.swift
+++ Sources/PaginationRequestType.swift
@@ -104,8 +104,10 @@ extension PaginationRequestType {
     public var URLRequest: NSMutableURLRequest {
         let mutableURLRequest = NSMutableURLRequest(URL: route.baseURL.URLByAppendingPathComponent(route.path))
         mutableURLRequest.HTTPMethod = route.method.rawValue
-        let params = (self as? URLRequestParametersSetup)?.urlRequestParametersSetup(mutableURLRequest, parameters: parameters) ?? parameters
+        var params = (self.route as? URLRequestParametersSetup)?.urlRequestParametersSetup(mutableURLRequest, parameters: parameters) ?? parameters
+        params = (self as? URLRequestParametersSetup)?.urlRequestParametersSetup(mutableURLRequest, parameters: params) ?? params
         let urlRequest = route.encoding.encode(mutableURLRequest, parameters: params).0
+        (self.route as? URLRequestSetup)?.urlRequestSetup(mutableURLRequest)
         (self as? URLRequestSetup)?.urlRequestSetup(mutableURLRequest)
         return urlRequest
     }
