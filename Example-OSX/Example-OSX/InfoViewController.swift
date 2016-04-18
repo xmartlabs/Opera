1c2b83911df6f021838133200795bbd91846cc00
Federico Ojeda <ojeda@xmartlabs.com>
2016-04-18 12:34:41 -0400

    Example application for Opera in OSX was created

---
+++ Example-OSX/Example-OSX/InfoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  InfoViewController.swift
+//  Example-OSX
+//
+//  Created by Federico Ojeda on 4/20/16.
+//  Copyright © 2016 Federico Ojeda. All rights reserved.
+//
+
+import Cocoa
+
+class InfoViewController: NSViewController {
+
+    var repository: Repository? = nil
+    
+    @IBOutlet weak var titleLabel: NSTextField!
+    @IBOutlet weak var companyLabel: NSTextField!
+    @IBOutlet weak var languageLabel: NSTextField!
+    @IBOutlet weak var openIssuesLabel: NSTextField!
+    @IBOutlet weak var forksLabel: NSTextField!
+    @IBOutlet weak var urlLabel: NSTextField!
+    
+    //outlets
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setValues()
+    }
+    
+    private func setValues() {
+        guard let repositoryToShow = repository else {
+            return
+        }
+        
+        titleLabel.stringValue = repositoryToShow.name
+        companyLabel.stringValue = repositoryToShow.company ?? "no info"
+        languageLabel.stringValue = repositoryToShow.language ?? "no info"
+        openIssuesLabel.stringValue = String(repositoryToShow.openIssues)
+        forksLabel.stringValue = String(repositoryToShow.forksCount)
+        urlLabel.stringValue = repositoryToShow.url.absoluteString
+    }
+    
+}
